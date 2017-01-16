290e5b49f421642fb49161e65318791fa8c0cd1d
richjdsmith <rich@richjdsmith.ca>
2017-01-15 23:49:31 -0800

    grid generates to near perfect squares. 'Near Perfect' being caused by margins/border of grid itself. Could fix, but that isn't the objective nore scope of this project

--- js/application.js
+++ js/application.js
@@ -1,44 +1,42 @@
-
-
 $(document).ready(function(){
 
-var gridWidth = 16;
-var height = 16;
-wrapperWidth = $(".wrapper").outerWidth();
 
+//Generating the grid of divs -  .block dimensions not set as of yet.
+var gridWidth = 16;
+var gridHeight = 16;
+wrapperWidth = $(".wrapper").outerWidth();
+wrapperHeight = $(".wrapper").outerHeight();
 createGrid = function (){
-    for (i = 0; i < height + 1; i++){
-        $("<div class='row'></div").appendTo(".grid");
-        
+    for (i = 0; i < gridHeight + 1; i++){
+        $("<div class='row'></div").appendTo(".grid");   
     }
 
     for (i = 1; i < gridWidth + 1; i++){
         $("<div class='block'><p></p></div>").appendTo(".row");
     }
-
-
-
 };
 createGrid();
 
-setBlockwidth = function(){
+//Setting .block dimension based on calculated height of grid.
+setBlockDimensions = function(){
     
-    var currentWidth = $(".block").outerWidth();
-    alert(currentWidth);
+    var currentWidth = $(".block").outerWidth(true);
+    var currentHeight = $(".block").outerHeight(true);
 
-    $(".block").css({
-        width: function(){
-            return (wrapperWidth + (currentWidth * gridWidth))/gridWidth;
-        }
-        },
-        {height: function(y){
-            return 960/height;
-            }
-        }
-    );
+    $(".block").css("width", function(){
+            return Math.round((wrapperWidth - (currentWidth * gridWidth))/(gridWidth + 2));
+        
+        });
+    $(".block").css("height", function(){
+            return Math.round($(".block").outerWidth());
+            });
 };
+setBlockDimensions();
 
-setBlockwidth();
+$(".row").css("height", function(){
+    return $(".block").outerHeight(true);
+});
+
 
 
 });
